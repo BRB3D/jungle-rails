098de845cc1efcf7655c87ff7f82083ae2f71dd3
BRB3D <samuraix221@hotmail.com>
2022-01-06 00:32:26 -0500

    daministrator dashbord counts shows categories and product counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @product_count = Product.count
+    @product_categories = Category.count
   end
 end
